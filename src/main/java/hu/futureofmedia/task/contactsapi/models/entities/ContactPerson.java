f6da7340524ea17a80c23f7ed28558b0bfddb64e
DESKTOP-BKVU3HR\kissd <kissdanilezzz@gmail.com>
2021-09-21 12:26:51 +0200

    create contact_persons table was added to setup.sql

--- src/main/java/hu/futureofmedia/task/contactsapi/models/entities/ContactPerson.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/models/entities/ContactPerson.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,6 +17,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "contact_persons")
 public class ContactPerson {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
