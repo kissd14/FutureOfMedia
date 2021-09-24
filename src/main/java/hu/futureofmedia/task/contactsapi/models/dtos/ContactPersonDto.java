49ef0bf7eb80507d464bb8bee4f656af148a9e6b
DESKTOP-BKVU3HR\kissd <kissdanilezzz@gmail.com>
2021-09-24 16:24:00 +0200

    @Schema added to dto

--- src/main/java/hu/futureofmedia/task/contactsapi/models/dtos/ContactPersonDto.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/models/dtos/ContactPersonDto.java
@@ -1,6 +1,7 @@
 package hu.futureofmedia.task.contactsapi.models.dtos;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -12,16 +13,26 @@ import lombok.Setter;
 @AllArgsConstructor
 @EqualsAndHashCode
 public class ContactPersonDto {
+  @Schema(description = "Contact person's id", accessMode = Schema.AccessMode.READ_ONLY)
   private Long id;
+  @Schema(description = "Contact person's first name")
   private String firstName;
+  @Schema(description = "Contact person's last name")
   private String lastName;
+  @Schema(description = "Contact person's company's primary key (id)")
   private Long companyId;
+  @Schema(description = "Contact person's company's name")
   private String companyName;
+  @Schema(description = "Contact person's email")
   private String email;
+  @Schema(description = "Contact person's phone number")
   private String phoneNumber;
+  @Schema(description = "Some note for contact person")
   private String note;
+  @Schema(description = "Date of user creation", accessMode = Schema.AccessMode.READ_ONLY)
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
   private LocalDateTime createdAt;
+  @Schema(description = "Date of user's last modification", accessMode = Schema.AccessMode.READ_ONLY)
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
   private LocalDateTime lastModifiedAt;
 }
