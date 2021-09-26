8c3a3edb1ad6462e8038bdb4c797f54526075e0d
DESKTOP-BKVU3HR\kissd <kissdanilezzz@gmail.com>
2021-09-26 19:55:01 +0200

    validation was added to ContactPersonDto

--- src/main/java/hu/futureofmedia/task/contactsapi/models/dtos/ContactPersonDto.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/models/dtos/ContactPersonDto.java
@@ -3,6 +3,10 @@ package hu.futureofmedia.task.contactsapi.models.dtos;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -13,17 +17,24 @@ import lombok.Setter;
 @AllArgsConstructor
 @EqualsAndHashCode
 public class ContactPersonDto {
+  @Positive
   @Schema(description = "Contact person's id", accessMode = Schema.AccessMode.READ_ONLY)
   private Long id;
+  @NotEmpty
   @Schema(description = "Contact person's first name")
   private String firstName;
+  @NotEmpty
   @Schema(description = "Contact person's last name")
   private String lastName;
+  @Positive
+  @NotNull
   @Schema(description = "Contact person's company's primary key (id)")
   private Long companyId;
   @Schema(description = "Contact person's company's name")
   private String companyName;
-  @Schema(description = "Contact person's email")
+  @Email
+  @NotEmpty
+  @Schema(description = "Contact person's email", accessMode = Schema.AccessMode.READ_ONLY)
   private String email;
   @Schema(description = "Contact person's phone number")
   private String phoneNumber;
